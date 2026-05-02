54cd78dd8d6c9150e77035236942ed02f2ca33d4
Vals43 <hei.teddy.3@gmail.com>
2026-05-02 18:09:39 +0300

    fix(esm): align through table column names for Session-Speaker many-to-many; specify foreignKey/otherKey to match migrations

--- models/index.js
+++ models/index.js
@@ -13,8 +13,8 @@ Session.belongsTo(Event, { foreignKey: 'eventId', as: 'event' });
 Session.belongsTo(Room, { foreignKey: 'roomId', as: 'room' });
 Room.hasMany(Session, { foreignKey: 'roomId', as: 'sessions' });
 
-Session.belongsToMany(Speaker, { through: 'SessionSpeakers', as: 'speakers' });
-Speaker.belongsToMany(Session, { through: 'SessionSpeakers', as: 'sessions' });
+Session.belongsToMany(Speaker, { through: 'SessionSpeakers', as: 'speakers', foreignKey: 'sessionId', otherKey: 'speakerId' });
+Speaker.belongsToMany(Session, { through: 'SessionSpeakers', as: 'sessions', foreignKey: 'speakerId', otherKey: 'sessionId' });
 
 Session.hasMany(Question, { foreignKey: 'sessionId', as: 'questions' });
 Question.belongsTo(Session, { foreignKey: 'sessionId', as: 'session' });
